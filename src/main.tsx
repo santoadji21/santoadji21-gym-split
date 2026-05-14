3662073368249a9ae8a8e35d5010b3c0ec6baf6b
santoadji21 <ahmad.aji.santoso21@gmail.com>
2026-05-14 21:06:10 +0700

    feat: add pwa

--- src/main.tsx
+++ src/main.tsx
@@ -5,3 +5,10 @@
 
   createRoot(document.getElementById("root")!).render(<App />);
   
+  if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch((error) => {
+      console.error("Service worker registration failed:", error);
+    });
+  });
+}
